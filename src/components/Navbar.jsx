17401835dc2be50971d57445cffd3068635f1d3f
Axel Liscovsky <axelliscovsky@outlook.com.ar>
2024-11-13 16:23:39 -0300

    Navbar movil dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,6 @@ const Navbar = () => {
       <div className="container mx-auto flex items-center justify-between py-4 px-6">
         {/* Logo */}
         <img src={logo_nav} alt="Tarjeta Pampeana" height={200} width={200} />
-
         {/* Botón para abrir/cerrar menú móvil */}
         <div className="md:hidden">
           <button onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)} className="text-purple-600 focus:outline-none">
@@ -26,9 +25,9 @@ const Navbar = () => {
         <div
           className={`${
             isMobileMenuOpen ? 'block' : 'hidden'
-          } md:flex md:space-x-8 text-lg font-regular absolute md:relative top-0 left-0 w-full md:w-auto bg-white md:bg-transparent transition-all duration-300 ease-in-out`}
+          } md:flex md:space-x-8 text-lg font-regular absolute md:relative top-0 left-0 w-full md:w-auto bg-white md:bg-transparent transition-all duration-300 ease-in-out sm:border-indigo-600 md:border-b-0 border-b-4 border-indigo-200`}
         >
-          <div className="flex flex-col md:flex-row items-center space-y-6 md:space-y-0 py-8 md:py-0 md:space-x-8">
+          <div className="flex flex-col md:flex-row items-center space-y-6 md:space-y-0 py-8 md:py-0 md:space-x-8 ">
             {/* Ícono para cerrar en el menú móvil */}
             <button
               onClick={() => setIsMobileMenuOpen(false)}
@@ -42,23 +41,23 @@ const Navbar = () => {
                 PARA VOS
               </a>
               {activeDropdown === 'paraVos' && (
-                <div className="absolute top-full left-0 bg-purple-600 text-white rounded-md shadow-lg p-4 flex flex-col whitespace-nowrap gap-2">
-                  <a href="#" className="hover:underline">
+                <div className="absolute top-full left-0 bg-purple-600 text-white rounded-md shadow-lg p-4 flex flex-col lg:flex-row h-16 whitespace-nowrap px-5 gap-4 dropNAV">
+                  <a href="#" className="hover:text-orange-300">
                     Para Vos
                   </a>
-                  <a href="#" className="hover:underline">
+                  <a href="#" className="hover:text-orange-300">
                     PampaCash
                   </a>
-                  <a href="#" className="hover:underline">
+                  <a href="#" className="hover:text-orange-300">
                     Dónde comprar
                   </a>
-                  <a href="#" className="hover:underline">
+                  <a href="#" className="hover:text-orange-300">
                     Costos
                   </a>
-                  <a href="#" className="hover:underline">
+                  <a href="#" className="hover:text-orange-300">
                     Medios de pago
                   </a>
-                  <a href="#" className="hover:underline">
+                  <a href="#" className="hover:text-orange-300">
                     Ahorrá Más
                   </a>
                 </div>
@@ -74,17 +73,17 @@ const Navbar = () => {
                 PARA TU COMERCIO
               </a>
               {activeDropdown === 'paraTuComercio' && (
-                <div className="absolute top-full left-0 bg-purple-600 text-white rounded-md shadow-lg p-4 flex flex-col whitespace-nowrap gap-2">
-                  <a href="#" className="hover:underline">
+                <div className="absolute top-full left-0 bg-purple-600 text-white rounded-md shadow-lg p-4 flex flex-col h-max lg:flex-row lg:h-16 max-h-64 lg:max-h-none overflow-y-auto whitespace-nowrap px-5 gap-4 dropNAV">
+                  <a href="#" className="hover:text-orange-300">
                     Servicios
                   </a>
-                  <a href="#" className="hover:underline">
+                  <a href="#" className="hover:text-orange-300">
                     Beneficios
                   </a>
-                  <a href="#" className="hover:underline">
+                  <a href="#" className="hover:text-orange-300">
                     Adhesión
                   </a>
-                  <a href="#" className="hover:underline">
+                  <a href="#" className="hover:text-orange-300">
                     Requisitos
                   </a>
                 </div>
