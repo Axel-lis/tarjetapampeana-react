293bfa3fa5693af943764d66ccc29289e0fb25e4
Axel Liscovsky <axelliscovsky@outlook.com.ar>
2024-11-16 16:36:50 -0300

    carousel modificado

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,7 +3,7 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import carousel1 from '../assets/banners/carousel1.png';
-
+import carousel1mobile from '../assets/banners/carousel1mobile.png';
 const Carousel = () => {
   const settings = {
     dots: true,
@@ -35,18 +35,23 @@ const Carousel = () => {
       <Slider {...settings}>
         {slides.map((slide, index) => (
           <div key={index} className="w-full flex flex-row">
-            <div className="w-full flex flex-row">
+            <div className="w-full flex flex-col justify-center md:flex-row">
+              {/* Imagen */}
+              <div className="w-full md:w-2/4 flex items-center justify-center order-1 md:order-2">
+                <img
+                  srcSet={`${carousel1mobile} 700w, ${carousel1} 1200w, ${carousel1} 1600w`}
+                  src={carousel1}
+                  alt={slide.title}
+                  className="w-full h-3/4 object-contain"
+                />
+              </div>
               {/* Texto */}
-              <div className="w-1/3 p-6 flex flex-col justify-center">
+              <div className="w-full md:w-1/3 p-6 flex flex-col justify-center order-2 md:order-1">
                 <h2 className="text-2xl md:text-3xl font-bold mb-4">{slide.title}</h2>
                 <p className="text-lg mb-6">{slide.description}</p>
                 <button className="px-6 py-2 bg-blue-600 text-white rounded-full hover:bg-blue-700">
                   {slide.buttonText}
                 </button>
-              </div>
-              {/* Imagen */}
-              <div className="w-2/3 flex items-center justify-center">
-                <img src={slide.imgSrc} alt={slide.title} className="w-full h-auto object-contain" />
               </div>
             </div>
           </div>
