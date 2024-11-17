00543f295bb65dbd270fc739be56a70ccf4b8f01
Axel Liscovsky <axelliscovsky@outlook.com.ar>
2024-11-17 01:25:47 -0300

    paquetes a pagina principal

--- src/components/PaginaPrincipal.jsx
+++ src/components/PaginaPrincipal.jsx
@@ -1,5 +1,6 @@
 import Carousel from './Carousel';
 import Carousel2 from './Carousel2';
+import PaquetePampeana from './PaquetePampeana';
 import PaquetesAcordion from './PaquetesAcordion';
 import IconosPosiblidades from './IconosPosibilidades';
 
@@ -9,6 +10,7 @@ const PaginaPrincipal = () => {
       <Carousel tiempoCarga={1100} />
       <IconosPosiblidades tiempoCarga={1000} />
       <Carousel2 tiempoCarga={1500} />
+      <PaquetePampeana />
       <PaquetesAcordion />
     </div>
   );
