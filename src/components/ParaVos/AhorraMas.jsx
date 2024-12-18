d17d36d5c2a65ef23b787960d9719a4241ea6c1b
Axel Liscovsky <axelliscovsky@outlook.com.ar>
2024-12-18 13:19:04 -0300

    para vos fix

--- src/components/ParaVos/AhorraMas.jsx
+++ src/components/ParaVos/AhorraMas.jsx
@@ -21,37 +21,39 @@ const AhorraMas = () => {
   }, []);
 
   return (
-    <div className="flex flex-col sm:flex-row items-center justify-center sm:justify-between px-4 sm:px-10">
-      {/* Texto */}
-      <div className="w-full sm:w-1/2 max-w-2xl sm:ml-10 sm:mr-auto mb-6 sm:mb-0">
-        <p className="text-xl sm:text-2xl font-semibold text-purple-700 dark:text-purple-600 mb-4">
-          Con Tarjeta Pampeana, cada día <strong>AHORRA MÁS</strong>
-        </p>
-        <p className="text-xl sm:text-2xl font-semibold text-purple-700/75 dark:text-purple-600/75 mb-4">
-          Con Tarjeta Pampeana, cada día <strong>AHORRA MÁS</strong>
-        </p>
-        <p className="text-xl sm:text-2xl font-semibold text-purple-700/50 dark:text-purple-600/50 mb-4">
-          Con Tarjeta Pampeana, cada día <strong>AHORRA MÁS</strong>
-        </p>
-        <p className="text-xl sm:text-2xl font-semibold text-purple-700/25 dark:text-purple-600/25 mb-4">
-          Con Tarjeta Pampeana, cada día <strong>AHORRA MÁS</strong>
-        </p>
-      </div>
+    <div className="w-3/4 mx-auto py-12">
+      <div className="flex flex-col sm:flex-row items-center justify-center sm:justify-between px-4 sm:px-10">
+        {/* Texto */}
+        <div className="w-full sm:w-1/2 max-w-2xl sm:ml-10 sm:mr-auto mb-6 sm:mb-0">
+          <p className="text-xl sm:text-2xl font-semibold text-purple-700 dark:text-purple-600 mb-4">
+            Con Tarjeta Pampeana, cada día <strong>AHORRA MÁS</strong>
+          </p>
+          <p className="text-xl sm:text-2xl font-semibold text-purple-700/75 dark:text-purple-600/75 mb-4">
+            Con Tarjeta Pampeana, cada día <strong>AHORRA MÁS</strong>
+          </p>
+          <p className="text-xl sm:text-2xl font-semibold text-purple-700/50 dark:text-purple-600/50 mb-4">
+            Con Tarjeta Pampeana, cada día <strong>AHORRA MÁS</strong>
+          </p>
+          <p className="text-xl sm:text-2xl font-semibold text-purple-700/25 dark:text-purple-600/25 mb-4">
+            Con Tarjeta Pampeana, cada día <strong>AHORRA MÁS</strong>
+          </p>
+        </div>
 
-      {/* Animación Lottie */}
-      <div
-        className="flex items-center justify-center sm:w-1/4 sm:max-w-lg ml-auto cursor-pointer"
-        onClick={handleNavigate}
-      >
-        {/* Usando lottie-player */}
-        <lottie-player
-          src="https://lottie.host/b258f142-39a5-494a-82e9-32275619905f/RbtlJUQNHA.json"
-          background="transparent"
-          speed="1"
-          style={{ width: '300px', height: '300px' }}
-          loop
-          autoplay
-        ></lottie-player>
+        {/* Animación Lottie */}
+        <div
+          className="flex items-center justify-center sm:w-1/4 sm:max-w-lg ml-auto cursor-pointer"
+          onClick={handleNavigate}
+        >
+          {/* Usando lottie-player */}
+          <lottie-player
+            src="https://lottie.host/b258f142-39a5-494a-82e9-32275619905f/RbtlJUQNHA.json"
+            background="transparent"
+            speed="1"
+            style={{ width: '300px', height: '300px' }}
+            loop
+            autoplay
+          ></lottie-player>
+        </div>
       </div>
     </div>
   );
