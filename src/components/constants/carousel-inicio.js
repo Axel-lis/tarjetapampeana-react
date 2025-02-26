3a110f203a014b22b1747850288cf7fe9f2b8a7e
Axel-lis <axelliscovsky@outlook.com.ar>
2025-02-26 14:25:31 -0300

    images

--- src/components/constants/carousel-inicio.js
+++ src/components/constants/carousel-inicio.js
@@ -3,7 +3,7 @@ export const slides = [
     id: 1,
     title: "Solicitá tu prestamo PAMPACA$H",
     description: "Podes invertir hasta $5.000.000 por frasco y hacer rendir tu plata con hasta 31% TNA. Simula tus frascos sin compromiso y descubrí cuánta plata extra podes ganar.",
-    image: "/src/assets/images/carousel1/1a.jpg", 
+    image: "/src/assets/images/carousel1/1b.jpg", 
     buttonText: "SOLICITARLO YA",
     url: "/form-solicitud-credito"
   },
@@ -11,7 +11,7 @@ export const slides = [
     id: 2,
     title: "Título del Slide 2",
     description: "Descripción del slide 2 con más detalles e información adicional.",
-    image: "/src/assets/images/carousel1/2a.jpg",
+    image: "/src/assets/images/carousel1/2b.jpg",
     buttonText: "Botón de acción",
     url: "/slide-2"
   },
@@ -19,7 +19,7 @@ export const slides = [
     id: 3,
     title: "Título del Slide 3",
     description: "Descripción del slide 3 con más detalles e información adicional.",
-    image: "/src/assets/images/carousel1/3a.jpg",
+    image: "/src/assets/images/carousel1/3b.jpg",
     buttonText: "Botón de acción",
     url: "/slide-3"
   },
@@ -27,7 +27,7 @@ export const slides = [
     id: 4,
     title: "Título del Slide 4",
     description: "Descripción del slide 4 con más detalles e información adicional.",
-    image: "/src/assets/images/carousel1/4a.jpg",
+    image: "/src/assets/images/carousel1/4b.jpg",
     buttonText: "Botón de acción",
     url: "/slide-4"
   },
@@ -35,7 +35,7 @@ export const slides = [
     id: 5,
     title: "Título del Slide 5",
     description: "Descripción del slide 5 con más detalles e información adicional.",
-    image: "/src/assets/images/carousel1/5a.jpg",
+    image: "/src/assets/images/carousel1/5b.jpg",
     buttonText: "Botón de acción",
     url: "/slide-5"
   },
